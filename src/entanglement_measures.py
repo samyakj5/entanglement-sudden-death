04d879cc377a65dc91c1c52ee3403ab7f12548dc
d0ofus0 <jainsamyak.2018@gmail.com>
2026-03-21 11:39:39 -0400

    Renamed src files and refactored clifford circuit notebook

---
+++ src/entanglement_measures.py
@@ -0,0 +1,14 @@
+import math
+from qiskit.quantum_info import DensityMatrix, negativity
+
+def log_negativity(dm, subsystem):
+    rho = DensityMatrix(dm)
+    negv = negativity(rho, subsystem)
+    return (math.log(2 * negv + 1, 2))
+import math
+from qiskit.quantum_info import DensityMatrix, negativity
+
+def log_negativity(dm, subsystem):
+    rho = DensityMatrix(dm)
+    negv = negativity(rho, subsystem)
+    return (math.log(2 * negv + 1, 2))
